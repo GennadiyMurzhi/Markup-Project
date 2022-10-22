0223e23a3d800215261d33a5714781918f983fac
Gennadiy <gennadiy.murzhi@gmail.com>
2022-10-22 18:59:09 +0300

    fix comment

--- lib/generate_list_item.dart
+++ lib/generate_list_item.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-///method to generate list items
+///function to generate list items
 List<String> generateListItems() {
   return List<String>.generate(Random().nextInt(50), (int index) => 'Item${index + 1}');
 }
